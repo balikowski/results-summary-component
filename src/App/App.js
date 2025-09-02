04ff5783b0849126e781fb427bb7ea5a61a48fe9
balikowski <balikowskid@gmail.com>
2025-09-02 12:28:47 +0200

    Setup configuration

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/* W APP będą wszystkie główne kompnenty */
+
+const App = () => {
+    return (
+        <div>
+            Hello world!
+        </div>
+    );
+}
+
+export default App;
